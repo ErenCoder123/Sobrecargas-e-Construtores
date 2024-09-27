b16ce14f48e6c87ddebdb9a8e66907b31bb5674e
Miguel Cardoso <migueltranslatorbr@gmail.com>
2024-09-26 21:42:55 -0300

    Sobrecargas e Construções

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Retangulo r1 = new Retangulo();
+Retangulo r2 = new Retangulo(5);
+Retangulo r3 = new Retangulo(4, 6);
+
+Console.WriteLine($"A área do retangulo r1 é igual a: {r1.CalcularArea()}");
+Console.WriteLine($"O períemtro do retangulo r1 é igual a: {r1.CalcularPerimetro()}");
+
+
+Console.WriteLine($"A área do retangulo r2 é igual a: {r2.CalcularArea()}");
+Console.WriteLine($"O períemtro do retangulo r2 é igual a: {r2.CalcularPerimetro()}");
+
+
+Console.WriteLine($"A área do retangulo r3 é igual a: {r3.CalcularArea()}");
+Console.WriteLine($"O períemtro do retangulo r3 é igual a: {r3.CalcularPerimetro()}");
+
+
+r3.Redimensionar(2);
+Console.WriteLine($"A área do retangulo r3 é igual a: {r3.CalcularArea()}");
+Console.WriteLine($"O períemtro do retangulo r3 é igual a: {r3.CalcularPerimetro()}");
+
+r2.Redimensionar(10, 15);
+Console.WriteLine($"A área do retangulo r2 é igual a: {r2.CalcularArea()}");
+Console.WriteLine($"O períemtro do retangulo r2 é igual a: {r2.CalcularPerimetro()}");
---
+++ Retangulo.cs
@@ -0,0 +1,54 @@
+public class Retangulo
+{
+    public double altura;
+    public double largura; 
+
+    public Retangulo()
+    {
+        altura = 0;
+        largura = 0;
+    }
+
+    public Retangulo(double valor)
+    {
+        altura = valor;
+        largura = valor;
+    }
+
+    public Retangulo(double altura, double largura)
+    {
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    public double CalcularArea()
+    {
+        return altura * largura;
+
+    }
+
+    public double CalcularPerimetro()
+    {
+        return largura + altura + largura + altura;
+    }
+
+    public void Redimensionar()
+    {
+
+    }   
+
+
+    public void Redimensionar(double fator)
+    {
+        altura  *= fator;
+        largura *= fator;
+
+    }
+
+    public void Redimensionar(double largura, double altura)
+    {
+        this.altura = altura;
+        this.largura = largura;
+    }
+}
+    
